cc2ea46c4a4380420c2d651180ece4f1397a7e4a
shenkns <shenkns@gmail.com>
2023-11-14 22:37:42 +0300

    [GR-152] Update for enhanced logging

--- Source/WidgetsSystem/WidgetsSystem.Build.cs
+++ Source/WidgetsSystem/WidgetsSystem.Build.cs
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright shenkns Widgets System Developed With Unreal Engine. All Rights Reserved 2023.
 
 using UnrealBuildTool;
 
@@ -7,6 +7,7 @@ public class WidgetsSystem : ModuleRules
 	public WidgetsSystem(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		CppStandard = CppStandardVersion.Cpp17;
 		
 		PrivateIncludePaths.AddRange(
 			new string[] 
